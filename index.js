81db00a8d1ee2cf0ed4f5ecc99dad4ced9f71ac3
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-01-31 21:51:27 +0530

    changes

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-let display=document.getElementById("display")
-function input(event){
-//    console.log(event.target.value)
-     display.value+=event.target.value
+let display = document.getElementById("display");
+function input(event) {
+  //    console.log(event.target.value)
+  display.value += event.target.value;
+}
+function mea() {
+  let exp = display.value;
+  let result = eval(exp);
+  display.value = result;
+}
+function del() {
+  display.value = display.value.slice(0, -1);
+}
+function allclear(){
+    display.value="";
 }
